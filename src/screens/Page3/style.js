d9d34dde9e3a9e356cd966b3178273236af39639
VictorLima01 <victor.lima.araujo@gmail.com>
2021-04-29 00:49:25 -0300

    Ajuste da margin e padding

--- src/screens/Page3/style.js
+++ src/screens/Page3/style.js
@@ -8,30 +8,31 @@ export const Container = styled.SafeAreaView`
 
 export const Scroller = styled.ScrollView`
     flex:1;
-    padding: 30px;
 `;
-
 export const HeaderArea = styled.View`
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+    background-color: #442357;
+    margin-bottom: 20px;
+    padding: 0;
 `;
 export const HeaderTitle = styled.Text`
     width: 100%;
-    font-size: 24px;
-    font-weight: bold;
+    font-size: 15px;
     color: #FFF;
-    margin-bottom: 20px
+    padding: 30px;
+    font-weight: bold;
+`;
+export const HeaderTitlePage = styled.Text`
+    width: 265px;
+    font-size: 22px;
+    color: #FFF;
+    padding: 30px;
 `;
 export const HeaderAreaPage = styled.View`
     justify-content: center;
     align-items: center;
-`;
-export const HeaderTitlePage = styled.Text`
-    width: 100%;
-    font-size: 15px;
-    color: #FFF;
-    margin-bottom: 20px
 `;
 export const ListArea = styled.View`
     margin-top: 30px;
@@ -43,12 +44,15 @@ export const Area = styled.TouchableOpacity`
     border-radius: 20px;
     padding: 15px;
     flex-direction: row;
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-top: 20px;
 `;
 export const InfoArea = styled.View`
     justify-content: space-between;
+    padding: 20px;
 
 `;
 export const InfoName = styled.Text`
-    font-size: 17px;
-    font-weight: bold;
+
 `;
