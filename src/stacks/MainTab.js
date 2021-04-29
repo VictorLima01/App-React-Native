bbf712539fd8b1cfd01c6dbf96f86fe6efd4ec88
VictorLima01 <victor.lima.araujo@gmail.com>
2021-04-29 00:50:45 -0300

    Ajuste do menu

--- src/stacks/MainTab.js
+++ src/stacks/MainTab.js
@@ -15,10 +15,10 @@ export default () => (
     <Tab.Navigator tabBar={props=><CustomTabBar {...props} />}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Page1" component={Page1} />
-        <Tab.Screen name="Page2" component={Page2} />
+        <Tab.Screen name="Gestao" component={Gestao} />
         <Tab.Screen name="Page3" component={Page3} />
         <Tab.Screen name="Profile" component={Profile} />
-        <Tab.Screen name="Gestao" component={Gestao} />
+        <Tab.Screen name="Page2" component={Page2} />
         <Tab.Screen name="FluxoCaixa" component={FluxoCaixa} />
     </Tab.Navigator>
 );
