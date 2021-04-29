e51f22a1a6409c3f19760dd11b71653c123df80c
VictorLima01 <victor.lima.araujo@gmail.com>
2021-04-29 00:49:00 -0300

    Ajuste da tela de perguntas frequentes

--- src/screens/Page3/index.js
+++ src/screens/Page3/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import {
-    SafeAreaView,
-    Text,
-    View,
-    StyleSheet,
-    Dimensions,
-    ScrollView, 
+   SafeAreaView,
+   Text,
+   View,
+   StyleSheet,
+   Dimensions,
+   ScrollView, 
 } from 'react-native';
 import {
     Container,
@@ -20,45 +21,74 @@ import {
     HeaderAreaPage
 } from './style';
 import  {Ionicons} from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
 
 export default () => {
     return (
         <Container>
             <Scroller>
-                <HeaderAreaPage>
-                    <HeaderTitlePage numberOfLines={2}>
-                    Home/ Conciliação Pagamento</HeaderTitlePage>    
-                </HeaderAreaPage> 
-                <HeaderArea>
+            <HeaderArea>
+                    <HeaderTitlePage style={{marginTop: 35}}  numberOfLines={2}>
+                    Perguntas</HeaderTitlePage >    
+           </HeaderArea>
+           <HeaderArea style={{marginTop: -45}}>
                     <HeaderTitle numberOfLines={2}>
-                    <Ionicons name="analytics" size={25} color="#FFF" />
-                    Conciliação Pagamento</HeaderTitle>    
-                </HeaderArea>
+                    <Ionicons style={{marginRight: 10}} name="help-circle-outline" size={20} color="#FFF" />
+                      Perguntas frequentes</HeaderTitle>    
+           </HeaderArea>
+             <Area>
+                 <InfoArea>
+                   <InfoName style={styles.titulo}>
+                   Controle de Taxas</InfoName>
+                   </InfoArea>
+                   <Ionicons style={styles.icon} name="chevron-forward-outline" size={35} color="#442357" /> 
+             </Area>
+             <Area>
+                 <InfoArea>
+                   <InfoName style={styles.titulo}>
+                   Fluxo de caixa</InfoName>
+                   </InfoArea>
+                   <Ionicons style={styles.icon} name="chevron-forward-outline" size={35} color="#442357" /> 
+             </Area>
             </Scroller>
         </Container>
     )
 }
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-      justifyContent: 'center',
-      alignItems: 'center',
-      textAlign: 'center',
-      padding: 10,
-    },
-    header: {
-      textAlign: 'center',
-      fontSize: 18,
-      padding: 16,
-      marginTop: 16,
-    },
-    arrow:{
-        marginTop: 15,
-        justifyContent: 'space-between',
-        position: 'absolute', 
-        marginRight: 20,
-        right: 0
-    },
-  });
+   container: {
+     flex: 1,
+     backgroundColor: 'white',
+     justifyContent: 'center',
+     alignItems: 'center',
+     textAlign: 'center',
+     padding: 10,
+   },
+   header: {
+     textAlign: 'center',
+     fontSize: 18,
+     padding: 16,
+     marginTop: 16,
+   },
+   lineStyle:{
+       borderWidth: 0.2,
+       borderColor:'grey',
+       justifyContent: 'center',
+       marginTop: 15
+  }, 
+  icon:{
+   marginTop: 15,
+   justifyContent: 'space-between',
+   position: 'absolute', 
+   marginRight: 20,
+   right: 0,
+   padding: 20,
+ }, titulo:{
+   color: "#442357",
+   fontSize: 20,
+ }
+ , descricao:{
+   color: "grey",
+   fontSize: 15,
+   marginTop: 15
+ }
+ });
